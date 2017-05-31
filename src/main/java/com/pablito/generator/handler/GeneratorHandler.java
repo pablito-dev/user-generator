e403fcfe83eebd4171da74ab0b662b259bcd5e61
Paweł Nowak <pavelvaremreis@gmail.com>
2017-05-31 18:25:53 +0200

    Add uinames request for sample user names

--- src/main/java/com/pablito/generator/handler/GeneratorHandler.java
+++ src/main/java/com/pablito/generator/handler/GeneratorHandler.java
@@ -1,9 +1,6 @@
 package com.pablito.generator.handler;
 
-import com.pablito.generator.model.GeographicPointModel;
-import com.pablito.generator.model.GeographicSquareModel;
-import com.pablito.generator.model.GoogleGeolocalizationModel;
-import com.pablito.generator.model.GoogleGeolocalizationResponseModel;
+import com.pablito.generator.model.*;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -29,6 +26,9 @@ public class GeneratorHandler {
     private static final String GOOGLE_ADDRESS_PARAM = "?address=";
     private static final String GOOGLE_REVERSE_PARAM = "?latlng=";
 
+    private static final String UINAMES_API_URL = "https://uinames.com/api/";
+    private static final String UINAMES_AMOUNT_PARAM = "?amount=";
+
     public Mono<ServerResponse> generateUsers(final ServerRequest request) {
         final Optional<String> cityParam = request.queryParam("city");
         final String sizeParam = request.queryParam("amount").orElse("10");
@@ -41,6 +41,7 @@ public class GeneratorHandler {
                     .retrieve()
                     .bodyToMono(GoogleGeolocalizationResponseModel.class)
                     .log()
+                    .filter(t -> t.getResults().size() > 0)
                     .map(i -> i.getResults().get(0));
 
 
@@ -51,10 +52,18 @@ public class GeneratorHandler {
                         .accept(MediaType.APPLICATION_JSON)
                         .retrieve()
                         .bodyToMono(GoogleGeolocalizationResponseModel.class)
+                        .filter(t -> t.getResults().size() > 0)
                         .map(t -> t.getResults().get(0))
-            ).repeat();
+            ).repeat(Integer.parseInt(sizeParam));
 
-            return ok().body(temp, GoogleGeolocalizationModel.class);
+            final Flux<UiNamesUserModel> users = WebClient.create(UINAMES_API_URL)
+                    .get()
+                    .uri(UINAMES_AMOUNT_PARAM + Integer.parseInt(sizeParam))
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToFlux(UiNamesUserModel.class);
+
+            return ok().body(users, UiNamesUserModel.class);
         }
         else {
             return badRequest().build();
