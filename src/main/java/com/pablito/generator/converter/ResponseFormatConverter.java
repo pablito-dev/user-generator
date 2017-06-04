e9b9d817d55f6f52a0dfa53fa0e3652031a33df2
Paweł Nowak <pavelvaremreis@gmail.com>
2017-06-04 22:24:58 +0200

    Add converter to response in different data types

---
+++ src/main/java/com/pablito/generator/converter/ResponseFormatConverter.java
@@ -0,0 +1,46 @@
+package com.pablito.generator.converter;
+
+import com.pablito.generator.model.domain.UserModel;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+/**
+ * Created by pavel_000 on 04/06/2017.
+ */
+@Component
+public class ResponseFormatConverter {
+
+    private static final String ADDRESS_IMPEX_SCRIPT_LINE = "INSERT_UPDATE Address; &addrID; firstname; lastname; " +
+            "streetname[unique = true]; town; country(isocode); " +
+            "postalcode[unique = true]; email; fax; phone1; owner(Agent.uid)\n";
+
+    public Mono<String> convertDataToAddressImpex(final Flux<UserModel> generatedUsers) {
+        return generatedUsers
+                .map(convertUserToImpex())
+                .reduce((i, j) -> i + "\n" + j)
+                .map(i -> ADDRESS_IMPEX_SCRIPT_LINE + i);
+    }
+
+    private Function<UserModel, String> convertUserToImpex() {
+        return i -> new StringBuilder()
+                .append(convertFieldToImpex(i.getFirstName() + "." + i.getLastName()))
+                .append(convertFieldToImpex(i.getFirstName()))
+                .append(convertFieldToImpex(i.getLastName()))
+                .append(convertFieldToImpex(i.getStreetName()))
+                .append(convertFieldToImpex(i.getTown()))
+                .append(convertFieldToImpex(i.getCountry()))
+                .append(convertFieldToImpex(i.getPostalCode()))
+                .append(convertFieldToImpex(i.getEmail()))
+                .append(convertFieldToImpex(i.getPhone1()))
+                .append(convertFieldToImpex(i.getFax()))
+                .append(convertFieldToImpex(i.getEmail()))
+                .toString();
+    }
+
+    private String convertFieldToImpex(final String field) {
+        return ";" + field;
+    }
+}
