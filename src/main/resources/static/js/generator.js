b677454ff9885aaf00c55c5f749429f5c16881c4
Paweł Nowak <pavelvaremreis@gmail.com>
2017-06-06 22:54:15 +0200

    Add vendor and domain .js file

---
+++ src/main/resources/static/js/generator.js
@@ -0,0 +1,6 @@
+function initialize() {
+    var input = document.getElementById('searchCity');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
