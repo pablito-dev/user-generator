bd03984e95878b0a6addfbe80d7362681b6d49c2
Paweł Nowak <pavelvaremreis@gmail.com>
2017-10-17 23:26:10 +0200

    Change index route to "/"

--- src/main/java/com/pablito/generator/GeneratorApplication.java
+++ src/main/java/com/pablito/generator/GeneratorApplication.java
@@ -3,13 +3,22 @@ package com.pablito.generator;
 import com.pablito.generator.handler.GeneratorHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ServletHttpHandlerAdapter;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.result.view.ViewResolver;
+import org.springframework.web.server.adapter.HttpWebHandlerAdapter;
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
+import org.thymeleaf.spring5.view.reactive.ThymeleafReactiveViewResolver;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.resources;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.RouterFunctions.toHttpHandler;
 
 @SpringBootApplication
 public class GeneratorApplication {
@@ -21,8 +30,8 @@ public class GeneratorApplication {
 	@Bean
 	RouterFunction<?> router(final GeneratorHandler generatorHandler) {
 		return resources("/**", new ClassPathResource("/static/"))
-					.andOther(route(GET("/generate"), generatorHandler::renderData)
-							.andRoute(GET("/index"), generatorHandler::renderIndex));
+				.andOther(route(GET("/generate"), generatorHandler::renderData)
+						.andRoute(GET("/"), generatorHandler::renderIndex));
 	}
 }
 
